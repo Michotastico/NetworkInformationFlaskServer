9890dc73fd5882f36a3a7353c4387a3ec0fe03b7
Michel Llorens <mllorens@dcc.uchile.cl>
2016-01-20 17:34:13 -0300

    Correct implementation of Database

--- Model/Database.py
+++ Model/Database.py
@@ -6,17 +6,7 @@ class Database():
 
     def insert(self, value):
         cursor = self.db.cursor()
-        value = value.replace("[", "{")
-        value = value.replace("]", "}")
-        value = value.replace("{", "-S-")
-        value = value.replace("}", "-E-")
 
-        value = value.replace(":", "---")
-        value = value.replace("(", "-SS-")
-        value = value.replace(")", "-EE-")
-        value = value.replace(" ", "")
-        #val = "INSERT INTO json_table (json_value) VALUES ("+value+")"
-        #val = "INSERT INTO json_table (json_value) VALUES ("+str(value)+")"
         cursor.execute("INSERT INTO json_table (json_value) VALUES (%s)", value)
 
         self.db.commit()
